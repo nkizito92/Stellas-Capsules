625f4b6d3e747b2f9eec02964f833db6c5e8ce2a
Kizito Njoku <nkizito57@gmail.com>
2022-06-23 22:24:43 -0500

    added active navigation links

--- src/components/navComponent/Navigation.js
+++ src/components/navComponent/Navigation.js
@@ -4,12 +4,15 @@ import { NavLink } from 'react-router-dom'
 const Navigation = () => {
     const active = (isActive) => {
         return {
-            backgroundColor: isActive ? "white" : "",
-            color: isActive ? "black" : ""
+            backgroundColor: isActive ? "#093d00" : "",
+            color: isActive ? "white" : ""
         }
     }
     return (
-        <NavLink to="/">Home</NavLink>
+        <div className='navBar'>
+           <NavLink to="/" style={({ isActive }) => active(isActive)}>Home</NavLink>
+           <NavLink to="/capsules" style={({ isActive }) => active(isActive)}>Capsules</NavLink>
+        </div>
         )
     }
 
