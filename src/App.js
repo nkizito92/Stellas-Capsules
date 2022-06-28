533fd6d145796c1ebd4ab82f5e32a59bf0fb68bd
Kizito Njoku <nkizito57@gmail.com>
2022-06-27 22:24:38 -0500

    imported capsules and viewcapsule components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Home from './components/index'
 import Navigation from './components/navComponent/Navigation';
+import Capsules from './components/capsulesComponent/Capsules'
+import ViewCapsule from "./components/capsulesComponent/ViewCapsule";
 import './App.css';
 
 function App() {
+  // make nav bump to a section in the page
   return (
-    <div>
+    <div className="mainCenter">
       <Router>
         <Navigation />
         <Routes>
           <Route exact path='/' element={<Home />}/>
+          <Route exact path='/capsules' element={<Capsules />}/>
+          <Route exact path='/capsules/:id' element={<ViewCapsule />}/>
         </Routes>
       </Router>
     </div>
